747dac380feec0055729d585240144ab14a8a3ba
Evgen986 <Evgen986@mail.ru>
2022-09-27 11:03:43 +0300

    Checkpoint

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,6 @@
 public class Test {
-    public static void main(String[] args) {
-        int [] a = {3, 5, 6, 7, 9};
-        int sum = 0;
-        for (int i=4; i>0; i--){
-            sum=sum+a[i];
-        }
-        System.out.println(sum);
+    static final int x=12;
+    public void go(final int x){
+        System.out.println(x);
     }
 }
